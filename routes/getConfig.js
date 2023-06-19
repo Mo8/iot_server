4b13f0d4594477b6aa1db456fb512e6208a7ceef
Gameplushy <victor_f@orange.fr>
2023-06-19 14:41:06 +0200

    Factorizing

--- routes/getConfig.js
+++ routes/getConfig.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var parameters = {
+    "tempReq": 10,
+    "connectionConfig": 2,
+    "connectionFreq": 30
+}
+
+
 /* GET home page. */
 router.get('/host/api/Esp32/:name?', function (req, res, next) {
-    res.json({
-        "tempReq": 10,
-        "connectionConfig": 2,
-        "connectionFreq": 30
-    })
+    res.json(parameters)
 });
 
 router.put('/host/api/Esp32/:name?', express.json(), function (req, res, next) {
     console.log(`For ${req.params.name} : [` + req.body.temperatures + ']');
-    res.json({
-        "tempReq": req.body.config.tempReq,
-        "connectionConfig": req.body.config.connectionConfig,
-        "connectionFreq": req.body.config.connectionFreq
-    })
+    res.json(parameters)
 });
 
 module.exports = router;
