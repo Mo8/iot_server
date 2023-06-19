788aeaabb0018ed18aec31c23bdddeb74eb14de8
Gameplushy <victor_f@orange.fr>
2023-06-19 14:16:13 +0200

    Add name parameter

--- routes/getConfig.js
+++ routes/getConfig.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/host/api/Esp32', function (req, res, next) {
+router.get('/host/api/Esp32/:name', function (req, res, next) {
     res.json({
         "tempReq": 10,
         "connectionConfig": 2,
@@ -10,8 +10,8 @@ router.get('/host/api/Esp32', function (req, res, next) {
     })
 });
 
-router.put('/host/api/Esp32', express.json(), function (req, res, next) {
-    console.log(req.body.temperatures);
+router.put('/host/api/Esp32/:name', express.json(), function (req, res, next) {
+    console.log(`For ${req.params.name} : [` + req.body.temperatures + ']');
     res.json({
         "tempReq": req.body.config.tempReq,
         "connectionConfig": req.body.config.connectionConfig,
