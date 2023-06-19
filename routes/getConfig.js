f20adeff8b8e2eeb7e3d5abd38a427f141ca37b6
Gameplushy <victor_f@orange.fr>
2023-06-19 11:36:57 +0200

    Begin put request

--- routes/getConfig.js
+++ routes/getConfig.js
@@ -10,4 +10,13 @@ router.get('/host/api/Esp32', function (req, res, next) {
     })
 });
 
+router.put('/host/api/Esp32', express.json(), function (req, res, next) {
+    console.log(req.body);
+    res.json({
+        "tempReq": 10,
+        "connectionConfig": 2,
+        "coonectionReq": 30
+    })
+});
+
 module.exports = router;
