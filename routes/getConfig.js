58f28f83b2bd52281d4377973b8fb250613f8450
Mo8 <juliancautela@gmail.com>
2023-06-22 12:35:50 +0200

    cron working and test latest date  to fetch from lorawan

--- routes/getConfig.js
+++ routes/getConfig.js
@@ -8,23 +8,13 @@ var parameters = {
     "connectionFreq": 30
 }
 
-
-/* GET home page. */
-router.get('/host/api/Esp32/:name?', function (req, res, next) {
-    res.json(parameters)
-});
-
-router.put('/host/api/Esp32/:name?', express.json(), function (req, res, next) {
-    console.log(`For ${req.params.name} : [` + req.body.temperatures + ']');
-    res.json(parameters)
-});
-
-router.get('/getMeasures', async function (req, res, next) {
+exports.getData = async function getData(after) {
     const response = await axios.get("https://eu1.cloud.thethings.network/api/v3/as/applications/soulmbengue-app-lorawansrv-1/packages/storage/uplink_message",
         {
             headers: { Authorization: "Bearer NNSXS.AFXIMSE6QXHFGBFXSYHMQQ6XFXJKDAOKRNFGHHI.N4WWBDZ7B7TNJA4IKJ6DGZAS6PNSRQBXZSWPFZT5ZSON52NGJW2A" },
-            params: { after: "2023-06-21T18:00:00Z" }
-        })
+            params: { after: after}
+        });
+    console.log("data :" ,response.data);
     const ans = "[" + response.data.replaceAll(/\{\"result\"/g, ",{\"result\"").substring(1) + "]"
     var j = JSON.parse(ans).map(r => r.result);
     j = j.filter(r => r.end_device_ids["device_id"] == "eui-a8404194a1875ff3")
@@ -40,7 +30,23 @@ router.get('/getMeasures', async function (req, res, next) {
     }
     )
     console.log(j)
-    res.json(j);
+    return j;
+}
+
+
+
+/* GET home page. */
+router.get('/host/api/Esp32/:name?', function (req, res, next) {
+    res.json(parameters)
 });
 
-module.exports = router;
+router.put('/host/api/Esp32/:name?', express.json(), function (req, res, next) {
+    console.log(`For ${req.params.name} : [` + req.body.temperatures + ']');
+    res.json(parameters)
+});
+
+router.get('/getMeasures', async function (req, res, next) {
+   // TODO
+});
+
+exports.router = router;
