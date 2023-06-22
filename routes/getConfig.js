de587ca98ee2bf20adaaa6458c8b0a0da654ee8a
Gameplushy <victor_f@orange.fr>
2023-06-22 10:05:39 +0200

    Started working on LoraWAN GET request

--- routes/getConfig.js
+++ routes/getConfig.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
@@ -16,6 +17,26 @@ router.get('/host/api/Esp32/:name?', function (req, res, next) {
 router.put('/host/api/Esp32/:name?', express.json(), function (req, res, next) {
     console.log(`For ${req.params.name} : [` + req.body.temperatures + ']');
     res.json(parameters)
+});
+
+router.get('/getMeasures', async function (req, res, next) {
+    const response = await axios.get("https://eu1.cloud.thethings.network/api/v3/as/applications/soulmbengue-app-lorawansrv-1/packages/storage/uplink_message", { headers: { Authorization: "Bearer NNSXS.AFXIMSE6QXHFGBFXSYHMQQ6XFXJKDAOKRNFGHHI.N4WWBDZ7B7TNJA4IKJ6DGZAS6PNSRQBXZSWPFZT5ZSON52NGJW2A" } })
+    const ans = "[" + response.data.replaceAll(/\{\"result\"/g, ",{\"result\"").substring(1) + "]"
+    var j = JSON.parse(ans).map(r => r.result);
+    j = j.filter(r => r.end_device_ids["device_id"] == "eui-a8404194a1875ff3")
+    j = j.map(r => {
+        return {
+            'measures': {
+                'conduct_SOIL': r.uplink_message.decoded_payload.conduct_SOIL,
+                'temp_SOIL': r.uplink_message.decoded_payload.temp_SOIL,
+                'water_SOIL': r.uplink_message.decoded_payload.water_SOIL
+            },
+            'timestamp': r.received_at
+        }
+    }
+    )
+    console.log(j)
+    res.json(j);
 });
 
 module.exports = router;
