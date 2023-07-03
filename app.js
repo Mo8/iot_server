df9b5fba2dc64550da29c79e83571f00c4b813b7
Mo8 <juliancautela@gmail.com>
2023-07-03 13:58:21 +0200

    esp32 save temp with timestamp+ mqtt subscribe

--- app.js
+++ app.js
@@ -4,30 +4,61 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cron = require("node-cron");
+const mqtt = require('mqtt')
+
 
 var indexRouter = require('./routes/index');
 var getRouter = require('./routes/getConfig');
 const mongoose = require('mongoose');
 var app = express();
 
-mongoose.connect('mongodb://root:example@localhost:27017/lorawan?authSource=admin');
+mongoose.connect('mongodb://root:example@localhost:27017/datas?authSource=admin');
 
-const Data = mongoose.model('Data', { measures: { conduct_SOIL: Number, temp_SOIL: Number, water_SOIL: Number }, timestamp: {type:Date,unique:true}, },);
+const LorawanData = mongoose.model('LorawanData', { measures: { conduct_SOIL: Number, temp_SOIL: Number, water_SOIL: Number }, timestamp: {type:Date,unique:true}, },);
 
 
+
+
+const protocol = 'mqtt'
+const host = "broker.hivemq.com" 
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const client = mqtt.connect(`${protocol}://${host}:${port}`, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  reconnectPeriod: 1000,
+})
+
+const topic = '/ynov/esp32-CAUT/in/+'
+
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
+})
+
+client.on('message', (topic, payload) => {
+  const decoded = JSON.parse(payload.toString());
+  console.log('Received Message:', topic, decoded);
+  const n = topic.split('/');
+  getRouter.addTemperatures(n[n.lenght-1],decoded.temperatures,decoded.config.tempFreq);
+})
 
 cron.schedule('0 */20 * * * *', async () => {
   console.log('cron data');
   var query;
   try{
-    query = await Data.findOne({}, {}, { sort: { 'timestamp' : -1 } });
+    query = await LorawanData.findOne({}, {}, { sort: { 'timestamp' : -1 } });
   }catch(e){
     console.log(e)
   }
   console.log("Get after : ",query != null ? new Date(query.timestamp.getTime() + 1) : new Date(Date.now() - (1000*60*60*24)));
   var query = await getRouter.getData(query != null ? new Date(query.timestamp.getTime() + 1) : new Date(Date.now() - (1000*60*60*24)));
   console.log(query.length);
-  Data.create(query).then(()=> console.log("meow")).catch((r)=> console.error(r));  
+  LorawanData.create(query).then(()=> console.log("fetch !")).catch((r)=> console.error(r));  
 });
 
 
